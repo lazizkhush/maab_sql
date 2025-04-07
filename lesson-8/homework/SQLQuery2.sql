2857caf76c1de010db93d2262502fb9170919eb6
laziz <khushmurodovlaziz@gmail.com>
2025-04-07 14:43:25 +0500

    lesson-8 update

---
+++ lesson-8/homework/SQLQuery2.sql
@@ -0,0 +1,80 @@
+CREATE TABLE Groupings
+(
+StepNumber  INTEGER PRIMARY KEY,
+TestCase    VARCHAR(100) NOT NULL,
+[Status]    VARCHAR(100) NOT NULL
+);
+INSERT INTO Groupings (StepNumber, TestCase, [Status]) 
+VALUES
+(1,'Test Case 1','Passed'),
+(2,'Test Case 2','Passed'),
+(3,'Test Case 3','Passed'),
+(4,'Test Case 4','Passed'),
+(5,'Test Case 5','Failed'),
+(6,'Test Case 6','Failed'),
+(7,'Test Case 7','Failed'),
+(8,'Test Case 8','Failed'),
+(9,'Test Case 9','Failed'),
+(10,'Test Case 10','Passed'),
+(11,'Test Case 11','Passed'),
+(12,'Test Case 12','Passed');
+
+--TASK 1
+SELECT MinStepNumber, MaxStepNumber, Status, Consecutive FROM
+(SELECT GroupingColumn, Status, 
+	MIN(StepNumber) as MinStepNumber,
+	MAX(StepNumber) as MaxStepNumber,
+	MAX(rn) as Consecutive
+From (SELECT *,
+		ROW_NUMBER() OVER(PARTITION BY STATUS ORDER BY StepNumber) AS rn,
+		StepNumber - ROW_NUMBER() OVER(PARTITION BY STATUS ORDER BY StepNumber) AS GroupingColumn
+	FROM Groupings) t
+GROUP BY GroupingColumn, Status) t
+ORDER BY MinStepNumber
+
+
+--TASK 2
+CREATE TABLE [dbo].[EMPLOYEES_N]
+(
+    [EMPLOYEE_ID] [int] NOT NULL,
+    [FIRST_NAME] [varchar](20) NULL,
+    [HIRE_DATE] [date] NOT NULL
+)
+ 
+INSERT INTO [dbo].[EMPLOYEES_N]
+VALUES
+	(1001,'Pawan','1975-02-21'),
+	(1002,'Ramesh','1976-02-21'),
+	(1003,'Avtaar','1977-02-21'),
+	(1004,'Marank','1979-02-21'),
+	(1008,'Ganesh','1979-02-21'),
+	(1007,'Prem','1980-02-21'),
+	(1016,'Qaue','1975-02-21'),
+	(1155,'Rahil','1975-02-21'),
+	(1102,'Suresh','1975-02-21'),
+	(1103,'Tisha','1975-02-21'),
+	(1104,'Umesh','1972-02-21'),
+	(1024,'Veeru','1975-02-21'),
+	(1207,'Wahim','1974-02-21'),
+	(1046,'Xhera','1980-02-21'),
+	(1025,'Wasil','1975-02-21'),
+	(1052,'Xerra','1982-02-21'),
+	(1073,'Yash','1983-02-21'),
+	(1084,'Zahar','1984-02-21'),
+	(1094,'Queen','1985-02-21'),
+	(1027,'Ernst','1980-02-21'),
+	(1116,'Ashish','1990-02-21'),
+	(1225,'Bushan','1997-02-21');
+
+SELECT 
+	CONCAT(HIRE_YEAR+1, '-' , (NEXT_YEAR-1)) AS YEARS
+FROM(
+	SELECT EMPLOYEE_ID, YEAR(HIRE_DATE) AS HIRE_YEAR,
+		LEAD(YEAR(HIRE_DATE)) OVER(ORDER BY YEAR(HIRE_DATE)) AS NEXT_YEAR
+	FROM EMPLOYEES_N) T
+WHERE HIRE_YEAR > 1975 AND (NEXT_YEAR-HIRE_YEAR) > 1
+ORDER BY HIRE_YEAR
+
+
+
+
